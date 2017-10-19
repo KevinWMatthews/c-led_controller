6d5cfe04d953fd9697cf471f4ee68c0e8529c233
Kevin Matthews <kmatthews@cyberdata.net>
2017-10-18 19:09:11 -0700

    Create and run tests

---
+++ tests/Test_LedController.cpp
@@ -0,0 +1,15 @@
+extern "C"
+{
+}
+
+#include "CppUTest/TestHarness.h"
+// #include "CppUTestExt/MockSupport.h"
+
+TEST_GROUP(LedController)
+{
+};
+
+TEST(LedController, wiring_check)
+{
+    FAIL("Start here!");
+}
