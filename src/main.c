de0ea533ad4b5e9d3281d1a884ea0c7ef4b7b6e5
Kevin Matthews <kmatthews@cyberdata.net>
2017-10-20 10:19:30 -0700

    Change how directories are included
    Watched more of a CMake tutorial.
    They recommend against using include_directories() and for using target_include_directories().
    That is, it is better to operate on a target than on a directory.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
+#include "LedControllerConfig.h"
 #include <stdio.h>
 
 int main(void)
 {
+    printf("LedController Version %d.%d\n", LedController_VERSION_MAJOR, LedController_VERSION_MINOR);
     printf("Blink!\n");
     return 0;
 }
