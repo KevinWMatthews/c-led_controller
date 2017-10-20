6ab7c30059725ea7c0ed33af2d66a349a0ea9088
Kevin Matthews <kmatthews@cyberdata.net>
2017-10-20 16:07:56 -0700

    Enable memory leak detection

--- src/Led.c
+++ src/Led.c
@@ -1,14 +1,22 @@
 #include "Led.h"
 #include <stddef.h>
+#include <stdlib.h>
+
+typedef struct LedStruct
+{
+    LEDHW_GPIO gpio;
+} LedStruct;
 
 Led Led_Create(LEDHW_GPIO gpio)
 {
+    Led self = calloc( 1, sizeof(*self) );
     LedHw_Init(gpio);
-    return NULL;
+    return self;
 }
 
 void Led_Destroy(Led self)
 {
+    free(self);
     return;
 }
 
