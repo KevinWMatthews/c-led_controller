1743022fb1172217b5a0f661f47bca1b343cd8bc
Kevin W Matthews <KevinWMatthews@gmail.com>
2017-10-27 07:01:30 -0700

    Experiment with refactoring

--- hw_attiny861/src/ATtiny861.c
+++ hw_attiny861/src/ATtiny861.c
@@ -11,33 +11,45 @@ void ATtiny861_GpioInit(void)
     PORTB = 0xff;
 }
 
+static void set_gpio_as_output(uint8_t * port, uint8_t pin)
+{
+    SBI(*port, pin);
+}
+
+static void set_gpio_high(uint8_t * port, uint8_t pin)
+{
+    SBI(*port, pin);
+}
+
 void ATtiny861_GpioSetAsOutput(ATTN861_GPIO gpio, GPIO_STATE initial_state)
 {
     switch (gpio)
     {
     case ATTN861_PA0:
-        SBI(DDRA, DDA0);
+        // This provides useful information
+        set_gpio_as_output(&DDRA, DDA0);
         if (initial_state == GPIO_LOW)
             CBI(PORTA, PORTA0);
         else
-            SBI(PORTA, PORTA0);
+            // This doesn't gain us much; SBI is pretty clear
+            set_gpio_high(&PORTA, PORTA0);
         break;
     case ATTN861_PA1:
-        SBI(DDRA, DDA1);
+        set_gpio_as_output(&DDRA, DDA1);
         if (initial_state == GPIO_LOW)
             CBI(PORTA, PORTA1);
         else
-            SBI(PORTA, PORTA1);
+            set_gpio_high(&PORTA, PORTA1);
         break;
     case ATTN861_PA2:
-        SBI(DDRA, DDA2);
+        set_gpio_as_output(&DDRA, DDA2);
         if (initial_state == GPIO_LOW)
             CBI(PORTA, PORTA2);
         else
-            SBI(PORTA, PORTA2);
+            set_gpio_high(&PORTA, PORTA1);
         break;
     case ATTN861_PB0:
-        SBI(DDRB, DDB0);
+        set_gpio_as_output(&DDRB, DDB0);
         break;
     }
 }
