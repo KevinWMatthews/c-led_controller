5e00b301dbe0a1865f0e4f38aa1029441e5e8a17
Kevin W Matthews <KevinWMatthews@gmail.com>
2017-11-13 06:38:39 -0800

    Expand function signatures for consistency

--- include/Led.h
+++ include/Led.h
@@ -13,21 +13,21 @@ typedef enum
 
 // Create a software abstraction for an LED
 Led Led_Create(LEDHW_LED_NUMBER);
-void Led_Destroy(Led *);
+void Led_Destroy(Led * self);
 
 typedef enum
 {
     LED_OFF = 0,
     LED_ON  = 1
 } LED_STATE;
-LED_STATE Led_GetState(Led);
+LED_STATE Led_GetState(Led self);
 
 // Turn an LED on.
 // Returns 0 on success, < 0 on failure
-LED_RETURN_CODE Led_TurnOn(Led);
+LED_RETURN_CODE Led_TurnOn(Led self);
 
 // Turn an LED off.
 // Returns 0 on success, < 0 on failure
-LED_RETURN_CODE Led_TurnOff(Led);
+LED_RETURN_CODE Led_TurnOff(Led self);
 
 #endif
