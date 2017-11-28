cb67e0b0d240ffcadcd56640ba87c4b1845835f7
Kevin W Matthews <KevinWMatthews@gmail.com>
2017-11-28 08:33:07 -0800

    Experiment with moving AVR targets to a function in the toolchain file

--- Toolchain-avr-ATtiny.cmake
+++ Toolchain-avr-ATtiny.cmake
@@ -17,3 +17,58 @@ set (CMAKE_FIND_ROOT_PATH ${AVR_TOOLCHAIN_HOME})
 set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+
+function(avr_cross_compile)
+    add_executable(LedController main.c)
+
+    # Include source code header files
+    target_include_directories(LedController PRIVATE "${PROJECT_SOURCE_DIR}/include")
+    # Include generated Config.h file
+    target_include_directories(LedController PRIVATE "${PROJECT_BINARY_DIR}/include")
+
+
+    set_target_properties(
+        LedController
+        PROPERTIES
+            COMPILE_FLAGS "-mmcu=${AVR_MCU} -Wall -Wstrict-prototypes -funsigned-bitfields -funsigned-char -fpack-struct -fshort-enums"
+            LINK_FLAGS "-mmcu=${AVR_MCU} -Wl,-Map,LedController.map"
+    )
+
+    #TODO I broke this somehow... before I started editing this time around.
+    add_custom_target(
+        disassemble
+        COMMAND
+            # avr-objdump <option(s)> <file(s)>
+            # -h, --[section-]headers  Display the contents of the section headers
+            # -S, --source             Intermix source code with disassembly
+            ${CMAKE_OBJDUMP} -h -S LedController > LedController.lst
+        DEPENDS
+            LedController
+    )
+
+    # use find_program?
+    add_custom_target(
+        size
+        COMMAND
+            ${AVR_SIZE} LedController
+        DEPENDS
+            LedController
+    )
+
+    # This should actually be a custom_command? This works for now.
+    # custom targets can be made directly (make hex)
+    # but they are always considered out-of-date; the hex image will be rebuilt every time!
+    add_custom_target(
+        hex
+        COMMAND
+            # avr-objcopy [option(s)] in-file [out-file]
+            # -j --only-section <name>         Only copy section <name> into the output
+            # -O --output-target <bfdname>     Create an output file in format <bfdname>
+            ${CMAKE_OBJCOPY} -j .text -j .data -O ihex LedController LedController.hex
+        DEPENDS
+            LedController
+    )
+
+    install (TARGETS LedController DESTINATION bin)
+endfunction()
