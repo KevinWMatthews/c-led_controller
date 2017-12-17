0a93dac9d5f875df05c9ef39be01e8f09edf75d0
Kevin W Matthews <KevinWMatthews@gmail.com>
2017-12-17 13:54:48 -0800

    Configure skeleton of Button module

---
+++ tests/Test_Button.cpp
@@ -0,0 +1,22 @@
+extern "C"
+{
+#include "Button.h"
+}
+
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(Button)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+};
+
+TEST(Button, create_a_button)
+{
+    FAIL("HI");
+}
