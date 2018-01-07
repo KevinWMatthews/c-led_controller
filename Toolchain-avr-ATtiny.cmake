7fb43477110d399fef094b320891c26dbe64fc5d
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-01-06 16:17:58 -0800

    Clean AVR files

--- Toolchain-avr-ATtiny.cmake
+++ Toolchain-avr-ATtiny.cmake
@@ -24,9 +24,9 @@ set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 #TODO make sure that we can find the toolchain - use find_path()? use find_program?
 
 function(avr_cross_compile)
-    ###################
-    # Set up elf file #
-    ###################
+    ######################
+    # Configure elf file #
+    ######################
     add_executable(LedController.elf main.c)
 
     target_include_directories(LedController.elf
@@ -41,15 +41,17 @@ function(avr_cross_compile)
             LINK_FLAGS "-mmcu=${AVR_MCU} -Wl,-Map,${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/LedController.map"
     )
 
+    # Create a list of files that we need to clean.
+    set(avr_clean_files "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/LedController.map")
 
 
-    ###################
-    # Set up hex file #
-    ###################
+
+    ######################
+    # Configure hex file #
+    ######################
     # Custom targets can be made directly ('make hex' works) but
     # they are always considered out-of-date; the hex image will be rebuilt every time!
-    # Cutsom targets to not generate output?
-    # This is ok for now but I do wonder about it.
+    # Custom targets do not build an OUTPUT file.
     add_custom_target(hex
         COMMAND
             # build the dependency
@@ -58,7 +60,7 @@ function(avr_cross_compile)
     )
 
     # Custom commands can not be invoked from the command line - 'make hex' will not work.
-    # Does/can generate output
+    # Instead, they specify how to build the OUTPUT file.
     add_custom_command(
         OUTPUT
             LedController.hex
@@ -69,13 +71,40 @@ function(avr_cross_compile)
             ${CMAKE_OBJCOPY} -j .text -j .data -O ihex "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/LedController.elf" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/LedController.hex"
         DEPENDS
             LedController.elf
+        COMMENT
+            "hex: Generating .hex file"
     )
 
+    list(APPEND avr_clean_files
+        "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/LedController.hex"
+    )
+
+    ###
+    # Alternatively, we can create a custom target and then attach a custom command to it.
+    #add_custom_target(hex
+    #    DEPENDS
+    #        LedController.elf
+    #    COMMENT
+    #        "hex target"
+    #)
+
+    #add_custom_command(
+    #    TARGET              # Attach this command to the specified target
+    #        hex
+    #    COMMAND
+    #        # avr-objcopy [option(s)] in-file [out-file]
+    #        # -j --only-section <name>         Only copy section <name> into the output
+    #        # -O --output-target <bfdname>     Create an output file in format <bfdname>
+    #        ${CMAKE_OBJCOPY} -j .text -j .data -O ihex "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/LedController.elf" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/LedController.hex"
+    #    COMMENT
+    #        "hex command"
+    #)
 
 
-    #############################
-    # Set up disassemble target #
-    #############################
+
+    ################################
+    # Configure disassemble target #
+    ################################
     add_custom_target(disassemble
         COMMAND
             # avr-objdump <option(s)> <file(s)>
@@ -84,13 +113,19 @@ function(avr_cross_compile)
             ${CMAKE_OBJDUMP} -h -S "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/LedController.elf" > "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/LedController.lst"
         DEPENDS
             LedController.elf
+        COMMENT
+            "disassemble: Generating .lst file"
+    )
+
+    list(APPEND avr_clean_files
+        "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/LedController.lst"
     )
 
 
 
-    ######################
-    # Set up size target #
-    ######################
+    #########################
+    # Configure size target #
+    #########################
     add_custom_target(size
         COMMAND
             ${AVR_SIZE} "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/LedController.elf"
@@ -98,10 +133,25 @@ function(avr_cross_compile)
             LedController.elf
     )
 
+
+
+    ###################
+    # Install targets #
+    ###################
     install(
         TARGETS
             LedController.elf
         DESTINATION
             bin
+    )
+
+
+
+    #########################
+    # Clean generated files #
+    #########################
+    set_directory_properties(
+        PROPERTY ADDITIONAL_MAKE_CLEAN_FILES
+            "${avr_clean_files}"
     )
 endfunction()
