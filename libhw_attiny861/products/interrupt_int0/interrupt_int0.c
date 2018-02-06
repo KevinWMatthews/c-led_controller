992692c8da75a205d3e727cca124fbdf9ffd0280
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-02-06 07:15:33 -0800

    Compiles; configure settings, basic interrupt vector

--- libhw_attiny861/products/interrupt_int0/interrupt_int0.c
+++ libhw_attiny861/products/interrupt_int0/interrupt_int0.c
@@ -1,4 +1,29 @@
+#include <avr/io.h>         // For register definitions
+#include <avr/interrupt.h>  // For sei()
+#include "BitManip.h"
+
 int main(void)
 {
+    // Generate an interrupt request for INT0
+    SBI(GIMSK, INT0);
+
+    // Set interrupt direction
+    SBI(MCUCR, ISC01);  // Falling edge
+    CBI(MCUCR, ISC00);
+
+    // Configure interrupt flag
+    //TODO Experiment with removing this.
+    SBI(GIFR, INTF0);
+
+    // Enable global interrupts
+    sei();
+
     return 0;
+}
+
+// Define an interrupt handler
+// See avr/interrupt.h for funciton declaration, avr/io.h for various vectors
+ISR(INT0_vect)
+{
+    ;
 }
