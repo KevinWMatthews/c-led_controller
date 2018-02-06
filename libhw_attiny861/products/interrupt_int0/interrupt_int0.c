aa56455a86debb870c859a2b03fc04825f36129d
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-02-06 07:37:49 -0800

    Toggle LED in an interrupt
    Will need debouncing.
    May implement this using a timer.
    
    I don't have a push-button yet, so my signal is
    definitely unclean. Hard to get consistent changes.
    
    Still, it works; ish.

--- libhw_attiny861/products/interrupt_int0/interrupt_int0.c
+++ libhw_attiny861/products/interrupt_int0/interrupt_int0.c
@@ -2,21 +2,34 @@
 #include <avr/interrupt.h>  // For sei()
 #include "BitManip.h"
 
+
 int main(void)
 {
+    // Enable LED, for testing
+    SBI(DDRA, DDA0);        // Set direction
+    SBI(PORTA, PORTA0);     // Drive output high
+
+    // Set interrupt pin as input with internal pull-up (I think)
+    CBI(DDRB, DDB6);        // Set direction
+    SBI(PORTB, PORTB6);     // Enable internal pull-up
+
     // Generate an interrupt request for INT0
     SBI(GIMSK, INT0);
 
-    // Set interrupt direction
-    SBI(MCUCR, ISC01);  // Falling edge
-    CBI(MCUCR, ISC00);
-
     // Configure interrupt flag
-    //TODO Experiment with removing this.
     SBI(GIFR, INTF0);
+
+    // Set interrupt direction
+    CBI(MCUCR, ISC01);
+    SBI(MCUCR, ISC00);
 
     // Enable global interrupts
     sei();
+
+    while (1)
+    {
+        ;   // Hang on!
+    }
 
     return 0;
 }
@@ -25,5 +38,5 @@ int main(void)
 // See avr/interrupt.h for funciton declaration, avr/io.h for various vectors
 ISR(INT0_vect)
 {
-    ;
+    PORTA ^= _BV(PORTA0);
 }
