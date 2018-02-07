562de41f99689c25b8b77a05db943dbe51e81bda
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-02-07 07:14:10 -0800

    Add Timer tests list and skeleton of Timer tests

---
+++ libhw_attiny861/tests/Test_ATtiny861Timer.cpp
@@ -0,0 +1,22 @@
+extern "C"
+{
+#include "ATtiny861Timer.h"
+}
+
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(ATtiny861Timer)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+};
+
+TEST(ATtiny861Timer, wiring_check)
+{
+    FAIL("start here");
+}
