21b43d7636f4e48998987f15539201562a52e058
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-02-08 07:13:41 -0800

    Refactor internal Timer0 code
    Write descriptive function names for creating the timer.

--- libhw_attiny861/src/ATtiny861Timer0.c
+++ libhw_attiny861/src/ATtiny861Timer0.c
@@ -2,16 +2,37 @@
 #include <avr/io.h>
 #include "BitManip.h"
 
+static void enable_8_bit_mode(void)
+{
+    SBI(TCCR0A, TCW0);
+}
+
+// CTC: Clear Timer on Compare match
+static void enable_clear_timer_on_compare_match(void)
+{
+    SBI(TCCR0A, CTC0);
+}
+
+static void enable_compare_match_A_interrupts(void)
+{
+    SBI(TIMSK, OCIE0A);
+}
+
+static void set_compare_A_match_value(uint8_t match_value)
+{
+    OCR0A = match_value;
+}
+
 ATTN861_TIMER0_STATUS_CODE ATtiny861Timer_Timer0_Create(ATtiny861Timer0_Params *params)
 {
     // Disable timer?
-    SBI(TCCR0A, TCW0);
-    SBI(TCCR0A, CTC0);
-    // CBI(TCCR0A, ICEN0);
-    SBI(TIMSK, OCIE0A);
-    OCR0A = params->match_value;
+    enable_8_bit_mode();
+    enable_clear_timer_on_compare_match();
+    // CBI(TCCR0A, ICEN0);  // Disable input capture mode.
+    enable_compare_match_A_interrupts();
+    set_compare_A_match_value(params->match_value);
     // Clear Timer0 prescaler?
-    // SBI(CS02
+    // Do not enable timer. Start() does this.
     return ATTN861_TIMER0_SUCCESS;
 }
 
