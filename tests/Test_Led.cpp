73cf2bff4583b852880042ec00638e8a3c0b6436
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-02-10 08:59:38 -0800

    Create test variable for Led2 state

--- tests/Test_Led.cpp
+++ tests/Test_Led.cpp
@@ -10,12 +10,14 @@ TEST_GROUP(Led)
     Led led1;
     Led led2;
     LED_RETURN_CODE ret;
-    LED_STATE state;
+    LED_STATE state1;
+    LED_STATE state2;
 
     void setup()
     {
         led1 = Led_Create(LED_1);
-        state = LED_OFF;
+        state1 = LED_OFF;
+        state2 = LED_OFF;
     }
 
     void teardown()
@@ -49,25 +51,25 @@ TEST(Led, can_destroy_a_null_led)
 
 TEST(Led, led_is_off_after_create)
 {
-    ret = Led_GetState(led1, &state);
+    ret = Led_GetState(led1, &state1);
 
     LONGS_EQUAL(LED_SUCCESS, ret);
-    LONGS_EQUAL(LED_OFF, state);
+    LONGS_EQUAL(LED_OFF, state1);
 }
 
 TEST(Led, can_turn_led1_on)
 {
     LONGS_EQUAL(LED_SUCCESS, Led_TurnOn(led1));
-    Led_GetState(led1, &state);
-    LONGS_EQUAL(LED_ON, state);
+    Led_GetState(led1, &state1);
+    LONGS_EQUAL(LED_ON, state1);
 }
 
 TEST(Led, can_turn_led1_off)
 {
     Led_TurnOn(led1);
     CHECK_EQUAL(LED_SUCCESS, Led_TurnOff(led1));
-    Led_GetState(led1, &state);
-    CHECK_EQUAL(LED_OFF, state);
+    Led_GetState(led1, &state1);
+    CHECK_EQUAL(LED_OFF, state1);
 }
 
 TEST(Led, can_turn_led2_on)
@@ -75,16 +77,16 @@ TEST(Led, can_turn_led2_on)
     Led_TurnOn(led2);
 
     CHECK_EQUAL(LED_SUCCESS, Led_TurnOff(led2));
-    Led_GetState(led2, &state);
-    CHECK_EQUAL(LED_OFF, state);
+    Led_GetState(led2, &state2);
+    CHECK_EQUAL(LED_OFF, state2);
 }
 
 TEST(Led, can_turn_led2_off)
 {
     Led_TurnOn(led2);
     CHECK_EQUAL(LED_SUCCESS, Led_TurnOff(led2));
-    Led_GetState(led2, &state);
-    CHECK_EQUAL(LED_OFF, state);
+    Led_GetState(led2, &state2);
+    CHECK_EQUAL(LED_OFF, state2);
 }
 
 TEST(Led, will_not_get_state_if_state_pointer_is_null)
