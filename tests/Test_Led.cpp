80ec5cfab9eb1dc9981237588935b63572308205
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-02-10 08:53:54 -0800

    Rename Led tests to distinguish between Led1 and Led2

--- tests/Test_Led.cpp
+++ tests/Test_Led.cpp
@@ -7,31 +7,31 @@ extern "C"
 
 TEST_GROUP(Led)
 {
-    Led led;
+    Led led1;
     LED_RETURN_CODE ret;
     LED_STATE state;
 
     void setup()
     {
-        led = Led_Create(LED_1);
+        led1 = Led_Create(LED_1);
         state = LED_OFF;
     }
 
     void teardown()
     {
-        Led_Destroy(&led);
+        Led_Destroy(&led1);
     }
 };
 
-TEST(Led, create_an_led)
+TEST(Led, can_create_led1)
 {
-    CHECK_TRUE(led != NULL);
+    CHECK_TRUE(led1 != NULL);
 }
 
 TEST(Led, can_destroy_an_led)
 {
-    Led_Destroy(&led);
-    POINTERS_EQUAL(NULL, led);
+    Led_Destroy(&led1);
+    POINTERS_EQUAL(NULL, led1);
 }
 
 TEST(Led, can_destroy_a_null_led)
@@ -41,29 +41,29 @@ TEST(Led, can_destroy_a_null_led)
 
 TEST(Led, led_is_off_after_create)
 {
-    ret = Led_GetState(led, &state);
+    ret = Led_GetState(led1, &state);
 
     LONGS_EQUAL(LED_SUCCESS, ret);
     LONGS_EQUAL(LED_OFF, state);
 }
 
-TEST(Led, can_turn_led_on)
+TEST(Led, can_turn_led1_on)
 {
-    LONGS_EQUAL(LED_SUCCESS, Led_TurnOn(led));
-    Led_GetState(led, &state);
+    LONGS_EQUAL(LED_SUCCESS, Led_TurnOn(led1));
+    Led_GetState(led1, &state);
     LONGS_EQUAL(LED_ON, state);
 }
 
-TEST(Led, can_turn_led_off)
+TEST(Led, can_turn_led1_off)
 {
-    Led_TurnOn(led);
-    CHECK_EQUAL(LED_SUCCESS, Led_TurnOff(led));
-    Led_GetState(led, &state);
+    Led_TurnOn(led1);
+    CHECK_EQUAL(LED_SUCCESS, Led_TurnOff(led1));
+    Led_GetState(led1, &state);
     CHECK_EQUAL(LED_OFF, state);
 }
 
 TEST(Led, will_not_get_state_if_state_pointer_is_null)
 {
-    ret = Led_GetState(led, NULL);
+    ret = Led_GetState(led1, NULL);
     LONGS_EQUAL(ret, LED_NULL_POINTER);
 }
