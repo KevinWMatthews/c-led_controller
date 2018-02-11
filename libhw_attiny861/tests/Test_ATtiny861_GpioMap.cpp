90bf7802d21209644ab283fcf47374a759dffb92
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-02-11 13:02:23 -0800

    Add skeleton of GpioMap tests

---
+++ libhw_attiny861/tests/Test_ATtiny861_GpioMap.cpp
@@ -0,0 +1,23 @@
+extern "C"
+{
+#include "ATtiny861_GpioMap.h"
+}
+
+#include "CppUTest/TestHarness.h"
+
+
+TEST_GROUP(ATtiny861_GpioMap)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+};
+
+TEST(ATtiny861_GpioMap, wiring_check)
+{
+    FAIL("start here");
+}
