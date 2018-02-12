c80c1988a78973230504f63d62a558a5df521b42
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-02-12 06:28:15 -0800

    Create AVR Led_BlinkTwo

--- products_avr/Led_Blink/Led_Blink.c
+++ products_avr/Led_Blink/Led_Blink.c
@@ -3,14 +3,17 @@
 
 int main(void)
 {
+    // Configure Timer0
     TCCR0A &= ~_BV(TCW0);   // Enable 8-bit mode
     TCCR0A |= _BV(CTC0);    // CTC mode; clear timer on compare match rather than overflowing
     TIMSK |= _BV(OCIE0A);   // Enable Compare Match A interrupts
     OCR0A = 255;            // Set Compare Register A match value: 1 MHz / (1024*255) = 3.83 Hz
 
+    // Configure PA0 as gpio
     DDRA |= _BV(DDA0);      // Set PA0 as output
     PORTA &= ~_BV(PORTA0);  // Set initial state low
 
+    // Start timer
     TCCR0B |= _BV(CS02) | _BV(CS00);    // Start timer with prescaler 1024
 
     // Enable interrupts
@@ -24,5 +27,6 @@ int main(void)
 
 ISR(TIMER0_COMPA_vect)
 {
+    // Toggle PA0
     PINA |= _BV(PINA0);
 }
--- products_avr/Led_BlinkTwo/Led_BlinkTwo.c
+++ products_avr/Led_BlinkTwo/Led_BlinkTwo.c
@@ -1,4 +1,38 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
 int main(void)
 {
+    // Configure Timer0
+    TCCR0A &= ~_BV(TCW0);   // Enable 8-bit mode
+    TCCR0A |= _BV(CTC0);    // CTC mode; clear timer on compare match rather than overflowing
+    TIMSK |= _BV(OCIE0A);   // Enable Compare Match A interrupts
+    OCR0A = 255;            // Set Compare Register A match value: 1 MHz / (1024*255) = 3.83 Hz
+
+    // Configure PA6 as gpio
+    DDRA |= _BV(DDA6);      // Set PA6 as output
+    PORTA |= _BV(PORTA6);   // Set initial state high
+
+    // Configure PA7 as gpio
+    DDRA |= _BV(DDA7);      // Set PA7 as output
+    PORTA &= ~_BV(PORTA7);  // Set initial state high
+
+    // Start timer
+    TCCR0B |= _BV(CS02) | _BV(CS00);    // Start timer with prescaler 1024
+
+    // Enable interrupts
+    sei();
+
+    while (1)
+        ;
+
     return 0;
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+    // Toggle PA6
+    PINA |= _BV(PINA6);
+    // Toggle PA7
+    PINA |= _BV(PINA7);
 }
