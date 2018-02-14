d4865eed9156282d913fad917bdd5a3f63e80630
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-02-14 07:53:56 -0800

    Update GPIO documentation

--- libhw_attiny861/include/ATtiny861_Gpio.h
+++ libhw_attiny861/include/ATtiny861_Gpio.h
@@ -10,9 +10,14 @@ typedef enum
     GPIO_HIGH       = 1
 } GPIO_STATE;
 
-// Initialize all GPIO on the ATtiny861.
-// Pins default to floating inputs.
-// This function leaves all pins as inputs but enables their internal pull-up resistors.
+/*
+ * Initialize all GPIO on the ATtiny861.
+ *
+ * By default, pins are floating inputs.
+ * This function leaves all pins as inputs but prevents them from floating by
+ * enableing their internal pull-up resistors, as specified in the ATtiny861
+ * datasheet, section 10.1.6.
+ */
 void ATtiny861_GpioInit(void);
 
 typedef enum
@@ -22,18 +27,24 @@ typedef enum
     ATTINY861_SUCCESS       = 0
 } ATTINY861_STATUS_CODE;
 
-// Set the given gpio as an output.
-// Specify if gpio should be driven high or low after initialization.
-// If an invalid pin or state is passed, does nothing.
+/*
+ * Set the given gpio as an output and set its initial state.
+ * If an invalid pin or state is passed, does nothing.
+ */
 ATTINY861_STATUS_CODE ATtiny861_GpioSetAsOutput(ATTINY861_PIN pin, GPIO_STATE initial_state);
 
-// Set the given pin to a new state.
-// If an invalid pin or state is passed, does nothing.
-//TODO what does this do if the gpio is an input? Re-read the manual....
+/*
+ * Set the given pin to a new state.
+ * If an invalid pin or state is passed, does nothing.
+ */
+//TODO what does this do if the gpio is an input? Enable the internal pull-up?
 ATTINY861_STATUS_CODE ATtiny861_GpioSetState(ATTINY861_PIN pin, GPIO_STATE state);
 
-// Get the state of the given pin.
-//TODO does it matter if the pin is an input or an output? Re-read the manual.
+/*
+ * Get the state of the given pin.
+ *
+ * Valid for inputs and outputs.
+ */
 GPIO_STATE ATtiny861_GpioGetState(ATTINY861_PIN pin);
 
 /*
