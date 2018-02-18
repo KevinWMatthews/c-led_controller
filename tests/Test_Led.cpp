361e9b5157f24e0e7c01f042fe35e3da567644cf
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-02-18 09:16:29 -0800

    Revise Led tests
    Stylistic changes, rearrange, delete dead test.

--- tests/Test_Led.cpp
+++ tests/Test_Led.cpp
@@ -49,12 +49,6 @@ TEST(Led, can_destroy_a_null_led)
     Led_Destroy(NULL);
 }
 
-TEST(Led, will_not_create_invalid_led)
-{
-    Led led = Led_Create(42);
-    CHECK_TRUE(led == NULL);
-}
-
 TEST(Led, led_is_off_after_create)
 {
     ret = Led_GetState(led1, &state1);
@@ -65,16 +59,21 @@ TEST(Led, led_is_off_after_create)
 
 TEST(Led, can_turn_led1_on)
 {
-    LONGS_EQUAL(LED_SUCCESS, Led_TurnOn(led1));
+    ret = Led_TurnOn(led1);
+
     Led_GetState(led1, &state1);
+    LONGS_EQUAL(LED_SUCCESS, ret);
     LONGS_EQUAL(LED_ON, state1);
 }
 
 TEST(Led, can_turn_led1_off)
 {
     Led_TurnOn(led1);
-    LONGS_EQUAL(LED_SUCCESS, Led_TurnOff(led1));
+
+    ret = Led_TurnOff(led1);
+
     Led_GetState(led1, &state1);
+    LONGS_EQUAL(LED_SUCCESS, ret);
     LONGS_EQUAL(LED_OFF, state1);
 }
 
@@ -82,16 +81,21 @@ TEST(Led, can_turn_led2_on)
 {
     Led_TurnOn(led2);
 
-    LONGS_EQUAL(LED_SUCCESS, Led_TurnOff(led2));
+    ret = Led_TurnOff(led2);
+
     Led_GetState(led2, &state2);
+    LONGS_EQUAL(LED_SUCCESS, ret);
     LONGS_EQUAL(LED_OFF, state2);
 }
 
 TEST(Led, can_turn_led2_off)
 {
     Led_TurnOn(led2);
-    LONGS_EQUAL(LED_SUCCESS, Led_TurnOff(led2));
+
+    ret = Led_TurnOff(led2);
+
     Led_GetState(led2, &state2);
+    LONGS_EQUAL(LED_SUCCESS, ret);
     LONGS_EQUAL(LED_OFF, state2);
 }
 
@@ -102,6 +106,7 @@ TEST(Led, can_turn_led1_on_and_led2_off)
 
     Led_GetState(led1, &state1);
     Led_GetState(led2, &state2);
+
     LONGS_EQUAL(LED_ON, state1);
     LONGS_EQUAL(LED_OFF, state2);
 }
@@ -110,18 +115,19 @@ TEST(Led, can_toggle_an_led_on)
 {
     ret = Led_Toggle(led1);
 
-    LONGS_EQUAL(LED_SUCCESS, ret);
     Led_GetState(led1, &state1);
+    LONGS_EQUAL(LED_SUCCESS, ret);
     LONGS_EQUAL(LED_ON, state1);
 }
 
 TEST(Led, can_toggle_an_led_on_and_off)
 {
     Led_Toggle(led1);
+
     ret = Led_Toggle(led1);
 
-    LONGS_EQUAL(LED_SUCCESS, ret);
     Led_GetState(led1, &state1);
+    LONGS_EQUAL(LED_SUCCESS, ret);
     LONGS_EQUAL(LED_OFF, state1);
 }
 
@@ -132,6 +138,7 @@ TEST(Led, can_toggle_two_leds)
 
     Led_GetState(led1, &state1);
     Led_GetState(led2, &state2);
+
     LONGS_EQUAL(LED_ON, state1);
     LONGS_EQUAL(LED_ON, state2);
 }
@@ -156,26 +163,22 @@ TEST(Led, can_toggle_two_leds_in_opposing_states)
 TEST(Led, will_not_get_state_if_state_pointer_is_null)
 {
     ret = Led_GetState(led1, NULL);
-    LONGS_EQUAL(ret, LED_NULL_POINTER);
+    LONGS_EQUAL(LED_NULL_POINTER, ret);
 }
 
-
-#if 0
-TEST(Led, will_not_create_invalid_led)
+TEST(Led, will_not_create_an_invalid_led)
 {
-    LED_NUMBER led_num = ATTN861_PA0+1; // and 0?
-    Led invalid = Led_Create(led_num);
-    CHECK_TRUE(invalid == NULL);
+    Led led = Led_Create(42);
+    CHECK_TRUE(led == NULL);
 }
-#endif
 
-TEST(Led, do_not_turn_on_null_led)
+TEST(Led, do_not_turn_on_a_null_led)
 {
     ret = Led_TurnOn(NULL);
     LONGS_EQUAL(LED_NULL_POINTER, ret);
 }
 
-TEST(Led, do_not_turn_off_null_led)
+TEST(Led, do_not_turn_off_a_null_led)
 {
     ret = Led_TurnOff(NULL);
     LONGS_EQUAL(LED_NULL_POINTER, ret);
@@ -184,6 +187,5 @@ TEST(Led, do_not_turn_off_null_led)
 TEST(Led, do_not_toggle_a_null_led)
 {
     ret = Led_Toggle(NULL);
-
     LONGS_EQUAL(LED_NULL_POINTER, ret);
 }
